f979d16cf33e1308e8204c48292f850b893778b2
Agios <thundereme@gmail.com>
2022-09-05 22:42:24 -0600

    basic decript from file

--- utils/dec.py
+++ utils/dec.py
@@ -1,3 +1,4 @@
+import re
 import string
 import random
 from typing import List
@@ -15,7 +16,7 @@ class characters:
         if len(key) != 16:
             raise Exception(ValueError("Incorrect AES key length. It must be 16"))
         self.__key = self.compr_inputs(key)
-        self.__saltkey = self.__key[0:2] + self.__key[-2:]
+        self.__div_word = b"xaelko"
 
     def encript_file(self, file, key=None) -> None:
         if key is None:
@@ -47,7 +48,7 @@ class characters:
         with open(out_file, "ab") as file_out:
             string = self.__salt_and_encript(string)
             file_out.write(string)
-            file_out.write(b"xaelko")
+            file_out.write(self.__div_word)
             
     def __salt_and_encript(self, string: str) -> tuple[bytes, bytes, bytes]:
         key = str(self.__key)
@@ -60,6 +61,11 @@ class characters:
         string = self.compr_inputs(string)
         etxt, tag = encript_cipher.encrypt_and_digest(string)
         return etxt + tag + nonce
+    
+    def __AES_decript(self, ciphertext: bytes, nonce: bytes, tag:bytes) -> str:
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+        data = cipher.decrypt_and_verify(ciphertext, tag)
+        return data
 
     def compr_inputs(self, inputs):
         if isinstance(inputs, bytes):
@@ -78,7 +84,7 @@ class characters:
     def __decifr(self, encript_text):
         self.__decript_cipher = AES.new(self.__key, AES.MODE_EAX, nonce=self.__nonce) # fix nonce
         plaintext = self.__decript_cipher.decrypt(encript_text)
-        return plaintext
+        return plaintext.decode()
     
     def random_password(self, size: int = 32) -> str:
         out = "/p"
@@ -122,7 +128,6 @@ class characters:
                     token = sufix
                 else:
                     continue
-
                 if data_dict.get(sitio) is None and token == "" and user != "" and password != "":
                     data_dict[sitio] = {num_users : {"user": user, "password": password}}
 
@@ -136,9 +141,75 @@ class characters:
                             break
         return data_dict
 
+    def __remove_salt(self, salty_list: list) -> list:
+        num_users = 0
+        sites = []
+        sitio = ""
+        user = ""
+        password = ""
+        token = ""
+        data_dict = dict()
+        
+        for item in salty_list:
+            new_line = item.rstrip()
+            prefix = new_line[:3]
+            sufix = new_line[3:-2] + new_line[-1]
+            
+            if prefix == self.__p_sitio:
+                sitio = sufix
+                if sitio not in sites:
+                    num_users = 0
+                sites.append(sitio)
+                user = ""
+                password = ""
+                token = ""
+                continue
+
+            elif prefix == self.__p_user:
+                user = sufix
+                num_users += 1
+                continue
+
+            elif prefix == self.__p_password:
+                password = sufix
+
+            elif prefix == self.__p_token:
+                token = sufix
+            else:
+                continue
+            
+            if data_dict.get(sitio) is None and token == "" and user != "" and password != "":
+                data_dict[sitio] = {num_users : {"user": user, "password": password}}
+
+            elif data_dict.get(sitio) and token == "" and user != "" and password != "":
+                data_dict[sitio][num_users] = {"user": user, "password": password}
+
+            elif data_dict.get(sitio) and token != "" and user != "" and password != "":
+                for number in data_dict[sitio]:
+                    if user is data_dict[sitio][number]["user"]:
+                        data_dict[sitio][number]["token"] = token
+                        break
+        return data_dict
+    
+    def load_and_decript_file(self, file, key=None) -> list:
+        out = []
+        if key is None:
+            key = self.__key
+        with open(file, "rb") as f:
+            lines = f.read()
+            words = lines.split(self.__div_word)
+            for word in words:
+                etxt = word[:-32]
+                tag = word[-32:-16]
+                nonce = word[-16:]
+                decript = r"/" + self.__AES_decript(etxt, nonce, tag).decode()[3:-1]
+                out.append(decript)
+        return self.__remove_salt(out)                
+
 key = b'Sixteen byte key'
 file = r"file.txt"
+file_encripted = r"file_encripted.bin"
 ch = characters(key)
 #ch.encript_file(file)
 file_text = ch.load_data(file)
-print(file_text)
+decripted_text = ch.load_and_decript_file(file_encripted)
