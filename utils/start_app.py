605b782fc50871d5a56ddc32d1fd5775a764b40b
Agios <thundereme@gmail.com>
2022-09-17 22:07:19 -0600

    Basic Modern GUI

--- utils/start_app.py
+++ utils/start_app.py
@@ -25,7 +25,16 @@ class start_app(EaD):
         self.place_in_center = 0.5
         self.place_width_porcentage = 0.125 #38
         self.place_width = int(self.place_width_porcentage * self.width)
-        self.font = "arial.ttf"
+        self.place_height_porcentage = self.place_width_porcentage/20
+        self.place_height = 5
+        
+        self.font = "Times"
+        self.font_size_h1 = 50
+        self.font_size_n = 16
+        self.title_font_color = "#d8de76"
+        self.button_create_fg_color = "#ff7777"
+        self.button_load_fg_color = "#87d2fa"
+        self.bg_color = "#1e1e1e"
         
         self.show = "*"
         self.__file = None
@@ -34,6 +43,14 @@ class start_app(EaD):
         if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
             self.win.destroy()
         self.state = start_app_state.CLOSED
+        
+    def __on_enter(self, button: tk.Button, bg: str, fg: str) -> None:
+        button["background"] = bg
+        button["foreground"] = fg
+    
+    def __on_leave(self, button: tk.Button, bg: str, fg: str) -> None:
+        button["background"] = fg
+        button["foreground"] = bg
     
     def __open_encrypted_file(self) -> None:
         text_file_extensions = ['*.bin']
@@ -48,33 +65,63 @@ class start_app(EaD):
     def init_window(self) -> None:
         self.win=tk.Tk()
         self.win.title("SecKey")
-        self.win.wm_attributes('-toolwindow', 'True')
+        self.win.iconphoto(False, tk.PhotoImage(file=r"resources\sk.png"))
+        #self.win.wm_attributes('-toolwindow', 'True') # Hide icon
         self.win.resizable(width=False, height=False)
         
         # Set the geometry of the window
         self.win.geometry(self.geometry)
 
         # Create a frame widget
-        frame=tk.Frame(self.win, width=self.width, height=self.height)
+        frame=tk.Frame(self.win,
+                       width=self.width,
+                       height=self.height,
+                       bg = self.bg_color)
+        frame.pack(fill = tk.X, padx=20, pady=20)
         frame.grid(row=0, column=0, sticky="NW")
+        #frame.config(bg = "#fcfcfc")
 
         # Create a label widget
-        label=tk.Label(self.win, text="SecKeys", font=self.font)
-        label.place(relx=self.place_in_center, rely=self.place_starting, anchor=CENTER)
-
-        # Buttons
-        create_file_button = ttk.Button(self.win, text="Create File", width = self.place_width)
-        create_file_button.place(relx=self.place_in_center, rely=self.place_starting+self.place_step, anchor=CENTER)
- 
-        Load_key_button = ttk.Button(self.win, text="Load File", width = self.place_width, command=self.__open_encrypted_file)
-        Load_key_button.place(relx=self.place_in_center, rely=self.place_starting+self.place_step*2, anchor=CENTER)
-
+        Seckeys_label=tk.Label(self.win, text="SecKeys",
+                       font=(self.font, self.font_size_h1),
+                       fg = self.title_font_color,
+                       bg = self.bg_color,
+                       anchor = "w"
+                       )
+        Seckeys_label.place(relx=self.place_in_center, rely=self.place_starting, anchor=CENTER)
+        
+        create_file_button = tk.Button(self.win,
+                                        text="Create File",
+                                        width = self.place_width,
+                                        height = self.place_height,
+                                        font=(self.font, self.font_size_n),
+                                        fg = self.button_create_fg_color,
+                                        bg = self.bg_color,
+                                        activebackground = self.button_create_fg_color,
+                                        activeforeground = self.bg_color,
+                                        border = 0
+                                        )
+        create_file_button.bind("<Enter>", lambda event: self.__on_enter(create_file_button,self.button_create_fg_color, self.bg_color))
+        create_file_button.bind("<Leave>", lambda event: self.__on_leave(create_file_button,self.button_create_fg_color, self.bg_color))
+        create_file_button.place(relx=self.place_in_center, rely=0.5, anchor=CENTER)
+        
+        Load_key_button = tk.Button(self.win, text="Load File",
+                                     width  = self.place_width,
+                                     height = self.place_height,
+                                     font = (self.font, self.font_size_n),
+                                     fg = self.button_load_fg_color,
+                                     bg = self.bg_color,
+                                     activebackground = self.button_load_fg_color,
+                                     activeforeground = self.bg_color,
+                                     border = 0,
+                                     command=self.__open_encrypted_file)
+        Load_key_button.bind("<Enter>", lambda event: self.__on_enter(Load_key_button,self.button_load_fg_color, self.bg_color))
+        Load_key_button.bind("<Leave>", lambda event: self.__on_leave(Load_key_button,self.button_load_fg_color, self.bg_color))
+        Load_key_button.place(relx=self.place_in_center, rely=0.825, anchor=CENTER)
+        
         self.win.protocol("WM_DELETE_WINDOW", self.__on_closing)
         self.win.mainloop()
         return self.__file
-        
-    def __get_font_shape(self, string: str, font: str = None, font_size: int = 12) -> tuple:
-        if font is None:
-            font = self.font
-        font_init = ImageFont.truetype(font, font_size)
-        return font_init.getsize(string)
+
+ejemplo = start_app()
+ejemplo.init_window()
