ea60996d799971d23ab396c5e00b1fa9743b2552
Agios <thundereme@gmail.com>
2022-09-19 23:35:50 -0600

    Gui

--- utils/GUI.py
+++ utils/GUI.py
@@ -1,11 +1,16 @@
-from verify_password import verify_password # Verify and decript
+from verify_password import verify_password, Verify_password_state # Verify and decript
 from start_app import start_app, start_app_state # Create or load
 import sys
 
-gui = start_app()
-vad = verify_password()
+def main():
+    gui = start_app()
+    vad = verify_password()
 
-file = gui.init_window()
-if gui.state == start_app_state.CLOSED:
-    sys.exit()
-code = vad.Validation(file)
+    file = gui.init_window()
+    if gui.state == start_app_state.CLOSED:
+        sys.exit()
+    code = vad.Validation(file)
+    if vad.state == Verify_password_state.RETURN_TO_MAIN:
+        main()
+
+main()
