3616af461a043a2803035d9f019d0c6295074999
Agios <thundereme@gmail.com>
2022-09-21 23:41:12 -0600

    Data window handling

--- utils/data_window.py
+++ utils/data_window.py
@@ -1,5 +1,7 @@
+import os
 import tkinter as tk
 
+from PIL import Image
 from enum import Enum, auto
 
 class data_window_state(Enum):
@@ -9,14 +11,37 @@ class data_window:
     def __init__(self) -> None:
         self.width  = 300
         self.height = 400
+        self.menu_width = 50
+        self.menu_weight = 400
+        self.menu_buttons_step = self.height // 6
+        self.buttons_width = 7
+        self.buttons_height = 4
+    
+    def printo(self):
+        print("button")
+        
+    def __create_buttons(self, window: tk.Frame, text: str, command, relx: float, rely: float) -> tk.Button:
+        button = tk.Button(window,
+                                text = text,
+                                width=self.buttons_width,
+                                height=self.buttons_height,
+                                #image=tk.PhotoImage(file=r"resources\menu.png"),
+                                command = command
+                                )
+        button.place(relx=relx, rely=rely)
+        return button
     
     def __create_menu(self) -> None:
-        self.menu = tk.Frame(self.win, width=100 ,background="#3a7ff6")
+        self.menu = tk.Frame(self.win, width=self.menu_width, height=self.menu_weight ,background="#3a7ff6")
         self.menu.pack(side = "left",  fill = tk.BOTH)
         
-        menu_button = tk.Button(self.menu, text = "aru")
-        menu_button.place(relx=0.1, rely=0.10)
+        menu_button = self.__create_buttons(self.menu, "menu", self.printo, 0, 0.1).pack()
+        add_button = self.__create_buttons(self.menu, "add", self.printo, 0, 0.2).pack()
+        save_button = self.__create_buttons(self.menu, "save", self.printo, 0, 0.3).pack()
+        key_button = self.__create_buttons(self.menu, "key", self.printo, 0, 0.4).pack()
+        config_button = self.__create_buttons(self.menu, "config", self.printo, 0, 0.5).pack()
         
+    
     def window(self):
         self.win=tk.Tk()
         screen_width  = self.win.winfo_screenwidth()
@@ -34,5 +59,7 @@ class data_window:
         
         self.win.mainloop()
 
+import os
+print(os.path.exists(r"resources"))
 A = data_window()
 A.window()
