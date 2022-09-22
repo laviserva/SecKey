673c6e95c359f5a248fa54755ddbfd9e3922634f
Agios <thundereme@gmail.com>
2022-09-22 17:29:19 -0600

    Create an image from array

--- utils/img_utils.py
+++ utils/img_utils.py
@@ -1,6 +1,11 @@
 from PIL import Image, ImageTk
+import numpy as np
 
-def resize_image(img, width=14, height=14) -> ImageTk:
+def resize_image(img: str, width=14, height=14) -> ImageTk:
         image = Image.open(img)
         resize = image.resize((width, height))
         return ImageTk.PhotoImage(resize)
+
+def from_array_to_img(array: np.ndarray):
+        img = Image.fromarray(array)
+        return ImageTk.PhotoImage(image = img)
