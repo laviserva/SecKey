9aa40030ea2402e476a2b5047bf55bb5a35000be
Agios <thundereme@gmail.com>
2022-09-24 23:37:52 -0600

    change color of images bg and fg

--- utils/img_utils.py
+++ utils/img_utils.py
@@ -7,8 +7,21 @@ def resize_image(img: str, width=14, height=14) -> ImageTk:
         return ImageTk.PhotoImage(resize)
 
 def from_array_to_img(array: np.ndarray):
-        img = Image.fromarray(array)
-        return ImageTk.PhotoImage(image = img)
+        return ImageTk.PhotoImage(Image.fromarray(array))
     
 def from_img_to_array(img) -> np.ndarray:
         return np.array(Image.open(img))
+
+def change_img_colors(path:str, fg: list, bg:str = None):
+    array = from_img_to_array(path)
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            if array[i,j,3] != 0:
+                array[i, j] = fg
+                continue
+            if bg is None:
+                continue
+            if array[i,j,3] == 0:
+                array[i,j,3] = bg
+            
+    return from_array_to_img(array)
