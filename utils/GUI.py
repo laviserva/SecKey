6e5e81eb989bc9d563733ac4510848fd224325cd
Agios <thundereme@gmail.com>
2022-10-26 18:40:15 -0600

    adding data window into main functionality

--- utils/GUI.py
+++ utils/GUI.py
@@ -1,5 +1,6 @@
 from verify_password import verify_password, Verify_password_state # Verify and decript
 from start_app import start_app, start_app_state # Create or load
+from data_window import run_gui_load_file_with_key
 import sys
 
 def main():
@@ -12,5 +13,7 @@ def main():
     code = vad.Validation(file)
     if vad.state == Verify_password_state.RETURN_TO_MAIN:
         main()
+    if vad.state == Verify_password_state.DESTROYED and code is not None:
+        run_gui_load_file_with_key(file, )
 
 main()
